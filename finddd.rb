a91189f5dfb32819a562bd87f5d86a99f2e50675
ykpythemind <yukibukiyou@gmail.com>
2025-06-21 14:59:19 +0900

    fix

--- finddd.rb
+++ finddd.rb
@@ -16,8 +16,8 @@ def run_osa_script(script)
     exit 1
   end
 
-  # logはstderrに出力されるので、それを返す
-  return stderr
+  # logはstderrに出力される
+  stderr
 end
 
 window_log = run_osa_script(<<~'OSA')
@@ -68,33 +68,31 @@ puts windows
 result_right = [] # [ window_id, x, y, w, h, path ]
 result_left = []
 
-home_finder_exists = windows.any? do |window|
-  window_path = window["window_path"]
-  window_path == "#{ENV["HOME"]}/"
+windows = windows.sort_by do |window|
+  [window["window_path"].start_with?("/Volumes/") ? 0 : 1, window["window_path"]]
 end
-windows = windows.sort_by { |window| window["window_path"] }
 
+mounted_drives = Dir.glob("/Volumes/*").select { |path| File.directory?(path) && !path.end_with?("Macintosh HD") }
+mounted_drives.reject! { |path| path.include?("TimeMachine.localsnapshots") || path.include?("Installer") || path.include?("バックアップ") || path.include?("ykpythemind-mbp") || path.include?("mac studio") }
+mounted_drives.reject! { |path| windows.any? { |window| window["window_path"].delete_suffix("/") == path } }
+puts "mounted_drives"
+puts mounted_drives
+
+mounted_drives.each_with_index do |mounted_drive, index|
+  windows.unshift({ "window_id" => :mounted_drive, "window_path" => mounted_drive })
+  break if index == 1 # max 2
+end
+
+home_finder_exists = windows.any? { |window| window["window_path"].delete_suffix("/") == ENV["HOME"] }
 unless home_finder_exists
   puts "Home Finder is not running"
   windows.unshift({ "window_id" => :finder })
 end
 
-mounted_drives = Dir.glob("/Volumes/*").select { |path| File.directory?(path) && !path.end_with?("Macintosh HD") }
-mounted_drives.reject! { |path| path.include?("TimeMachine.localsnapshots") || path.include?("Installer") || path.include?("バックアップ") || path.include?("ykpythemind-mbp") || path.include?("mac studio") }
-mounted_drives = mounted_drives.slice(0, 2)
-puts "mounted_drives"
-puts mounted_drives
-
-mounted_drives.each_with_index do |mounted_drive, index|
-  windows[1 + index] = { "window_id" => :mounted_drive, "window_path" => mounted_drive }
-end
-
-# 4つまでが限界とする
-4.times.each_with_index do |index|
-  window = windows[index] || {}
-
+windows.each_with_index do |window, index|
   window_id = window["window_id"] || :finder
 
+  # 整理して出すのは4つまでが限界とする
   case index
   when 0
     result_left << [window_id, 0, 0, w / 2, h / 2, window["window_path"]]
@@ -104,6 +102,9 @@ end
     result_right << [window_id, w / 2, 0, w, h / 2, window["window_path"]]
   when 3
     result_right << [window_id, w / 2, h / 2, w, h, window["window_path"]]
+  when 4..windows.size
+    diff = 40 * (index - 3)
+    result_right << [window_id, w / 2 , h / 2 + diff + 50, (w * 0.8).to_i, (h * 0.8).to_i, window["window_path"]]
   end
 end
 
